6e2c61038831139bc423b162c20aa736f0027434
Maksym Kovalenko <maksymkovalenko@macbook-pro-maksym.home>
2018-12-21 17:04:41 +0100

    creating login form, and login/home pages

---
+++ src/component/forms/LoginForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Form, Button, Label} from "semantic-ui-react";
+
+class LoginForm extends React.Component {
+    state ={
+        data: {
+           email: '',
+           password: '',
+        },
+        loading:false,
+        erros: {} 
+    };
+
+    onChange = e => 
+    this.setState({
+        data: {...this.state.data, [e.target.name]: e.target.value }
+    });
+
+
+    render (){
+        const {data} = this.state;
+        return(
+            <Form>
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Example@example.com" value={data.email}
+                    value ={data.email}
+                    onChange={this.onChange}
+                    ></input>
+                </Form.Field>
+
+                <Button primary>
+                    Login
+                </Button>
+            </Form>
+        );
+    }
+}
+
+
+
+
+export default LoginForm;
---
+++ src/component/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginForm from '../forms/LoginForm';
+
+const LoginPage = () => <div>
+ <h1>Login Page</h1>
+
+ <LoginForm></LoginForm>
+</div>;
+
+export default LoginPage;
